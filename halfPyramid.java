2493dc33c07c91a36808bfa1f71ff9dc9d878484
Hemant <hemantkumar29052001@gmail.com>
2023-08-05 10:52:04 +0530

    done Half Pyramid * printing problem

---
+++ halfPyramid.java
@@ -0,0 +1,19 @@
+// Printing Half Pyramid *
+
+public class halfPyramid {
+    public static void main(String[] args){
+        int rows = 4;
+        int columns = 5;
+
+        // for printing Rows
+        for( int i=1; i <= rows; i++)
+        {
+            // for printing Columns
+            for( int j=1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
